a5e4f9ef43a8f6b715d1d4311a9851ac79dc2461
elpe2005 <Epedersen2005@gmail.com>
2023-11-24 11:02:05 +0100

    klar

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Console.WriteLine("Skriv in ett ord sen efter det skriv in hur många gånger du vill repetera det, max 9 gånger.");
+string ogstring = Console.ReadLine();
+Console.Write("Skriv in hur många gånger ");
+int repetera = Convert.ToInt32(Console.ReadLine());
+string repeterad = Console.ReadLine();
+if (ogstring.Length <= 9){
+    if (repetera <= 9){
+        for (int i = 0; i < repetera; i++){
+        repeterad += ogstring;
+        }
+        }
+        else{
+            Console.WriteLine("Inte repetera mer än 9 gånger!");
+        }
+        }
+        else{
+            Console.WriteLine("Ditt ord får inte innehålla mer än 9 bokstäver");
+        }
+        Console.WriteLine(repeterad);
+        Console.ReadKey();
+    
---
+++ Uppgift2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+bool kör = true;
+int första = 1;
+
+Console.WriteLine("Skriv in numret för N ");
+int tal1 = int.Parse(Console.ReadLine());
+int talSvar = 1;
+while (kör)
+{
+    
+}
